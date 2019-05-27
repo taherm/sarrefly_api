46dfb7f0a7e20d716d8525ac72f42a87cb10c11c
Taher Fakhruddin Makadam <mukadam.taher@gmail.com>
2019-05-27 22:18:43 +0300

    changes

--- database/migrations/2019_05_18_113636_create_drivers_table.php
+++ database/migrations/2019_05_18_113636_create_drivers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDriverTable extends Migration
+class CreateDriversTable extends Migration
 {
     /**
      * Run the migrations.
