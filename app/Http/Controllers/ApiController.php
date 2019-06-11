0b2bc6d36af66e799a44400092d903ebd81a81e7
Taher Fakhruddin Makadam <mukadam.taher@gmail.com>
2019-06-11 13:50:57 +0300

    changes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -52,7 +52,7 @@ class ApiController extends Controller
 
     public function login(Request $request)
     {
-        $val = User::where('name', $request->name)->where('password', $request->password)->first();
+        $val = \App\User::where('name', $request->name)->where('password', $request->password)->first();
         //return (string)(!empty($val));
         return $val;
     }
